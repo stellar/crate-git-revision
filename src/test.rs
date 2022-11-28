04b0601610bd75598461a49596670cd8f4d53a6c
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-11-28 12:54:03 -0800

    Remove opinionated rerun on changes to dir (#5)

--- src/test.rs
+++ src/test.rs
@@ -10,9 +10,12 @@ fn test_init() {
     assert!(res.is_ok());
     let out = str::from_utf8(&out).unwrap();
     println!("{out}");
-    assert!(
-        Regex::new("cargo:rustc-env=GIT_REVISION=[0-9a-f]+(-dirty)?")
-            .unwrap()
-            .is_match(out)
-    );
+    assert!(Regex::new(
+        "cargo:rerun-if-changed=.git/index
+cargo:rerun-if-changed=.git/HEAD
+cargo:rerun-if-changed=.git/refs
+cargo:rustc-env=GIT_REVISION=[0-9a-f]+(-dirty)?"
+    )
+    .unwrap()
+    .is_match(out));
 }
