d0c84e9291fad312a371fd1aebcf61df855fae76
Leigh <351529+leighmcculloch@users.noreply.github.com>
2026-05-27 13:47:55 +1000

    update build script documentation

--- src/lib.rs
+++ src/lib.rs
@@ -78,7 +78,7 @@
 //!
 //! ### Build scripts
 //!
-//! Requires the use of a build.rs build script. See [Build Scripts]() for more
+//! Requires the use of a build.rs build script. See [Build Scripts] for more
 //! details on how Rust build scripts work.
 //!
 //! [Build Scripts]: https://doc.rust-lang.org/cargo/reference/build-scripts.html
